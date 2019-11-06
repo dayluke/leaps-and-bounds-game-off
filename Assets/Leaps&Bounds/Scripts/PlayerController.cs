f8c174b7884ca3346eacd3f46cba5093b829284a
Luke Day <ld227707@falmouth.ac.uk>
2019-11-06 01:44:49 +0000

    Drastically improved the player movement script, implementing smooth jumping. Added ground detection through using a layer mask (platform is now on 'Ground' layer).

--- Assets/Leaps&Bounds/Scripts/PlayerController.cs
+++ Assets/Leaps&Bounds/Scripts/PlayerController.cs
@@ -4,15 +4,60 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public int speed;
+
+    public int moveSpeed = 100;
+    public int jumpForce = 400;
+
+    public LayerMask groundLayers;
+    public Transform groundCheck;
+
+    private float horizontalMovement = 0;
+    private float movementSmoothing = 0.05f;
+    private Vector3 zeroVector = Vector3.zero;
+    private Rigidbody2D rb = null;
+    private bool isGrounded = true;
+    private bool jumpPressed = false;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
 
     private void Update()
     {
-        float x = Input.GetAxis("Horizontal");
-        float y = Input.GetAxis("Vertical");
+        horizontalMovement = Input.GetAxisRaw("Horizontal") * moveSpeed;
+        
+        if (Input.GetButtonDown("Jump"))
+        {
+            jumpPressed = true;
+        }
+    }
 
-        Vector2 dir = new Vector3(x,y);
+    private void FixedUpdate()
+    {
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(groundCheck.position, 0.2f, groundLayers);
+        foreach (Collider2D coll in colliders)
+        {
+            if (coll.gameObject != gameObject)
+            {
+                isGrounded = true;
+            }
+        }
 
-        GetComponent<Rigidbody2D>().AddForce(dir * speed);
+        Move();
+    }
+
+    private void Move()
+    {
+        Vector3 targetVelocity = new Vector2(horizontalMovement * Time.fixedDeltaTime * 10f, rb.velocity.y);
+
+        rb.velocity = Vector3.SmoothDamp(rb.velocity, targetVelocity, ref zeroVector, movementSmoothing);
+
+        if (isGrounded && jumpPressed)
+        {
+            isGrounded = false;
+            jumpPressed = false;
+            rb.AddForce(new Vector2(0f, jumpForce));
+        }
     }
 }
