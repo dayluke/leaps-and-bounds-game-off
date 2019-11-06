f205231535c6482e2caac3426a088932762ea52c
Luke Day <ld227707@falmouth.ac.uk>
2019-11-06 00:07:21 +0000

    Added basic player functionality: enabling the player to move a (currently box) sprite on a simple platform.

---
+++ Assets/Leaps&Bounds/Scripts/PlayerController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public int speed;
+
+    private void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        Vector2 dir = new Vector3(x,y);
+
+        GetComponent<Rigidbody2D>().AddForce(dir * speed);
+    }
+}
